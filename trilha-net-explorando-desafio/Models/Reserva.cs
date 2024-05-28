bcc8d56595ba3a90631ae56b8131f70d79a4cdbb
ArmandoMartins1 <armando.amds97@gmail.com>
2024-05-28 14:48:35 -0300

    Desafio concluido hotelaria

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,63 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido // RESOLVIDO
+            // *IMPLEMENTE AQUI*
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido // RESOLVIDO
+                // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade da suite é menor do que o numero de hospedes.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes) // RESOLVIDO
+            // *IMPLEMENTE AQUI*
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+
+            // TODO: Retorna o valor da diária // RESOLVIDO
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            
+            decimal valor = DiasReservados * Suite.ValorDiaria; 
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10% // RESOLVIDO
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor*0.10M);
+            }
+
+            return valor;
+        }
+    }
+}
